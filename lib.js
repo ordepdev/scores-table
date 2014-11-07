6174f86a10d6ff0798b8769674858ad2e6aa92da
Pedro Tavares <pedrotavares.dev@gmail.com>
2014-11-07 14:49:36 +0000

    column aligns

--- lib.js
+++ lib.js
@@ -4,7 +4,8 @@ module.exports.options = {
   chars: { 'top': '' , 'top-mid': '' , 'top-left': '' , 'top-right': ''
          , 'bottom': '' , 'bottom-mid': '' , 'bottom-left': '' , 'bottom-right': ''
          , 'left': '' , 'left-mid': '' , 'mid': '' , 'mid-mid': ''
-         , 'right': '' , 'right-mid': '' , 'middle': ' ' }
+         , 'right': '' , 'right-mid': '' , 'middle': ' ' },
+  colAligns: ['left', 'right', 'middle', 'left']
 }
 
 module.exports.colorify = function (status) {
